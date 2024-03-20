a3508603664b5b6003a801421772dee3e22ccb93
fake150907 <yungayeon223@gmail.com>
2024-03-20 21:43:23 +0900

    재료공구 write.jsp 퍼블리싱 완료

--- src/main/java/com/sla/project/controller/UsrGroupBuyingController.java
+++ src/main/java/com/sla/project/controller/UsrGroupBuyingController.java
@@ -135,7 +135,8 @@ public class UsrGroupBuyingController {
 
 	@RequestMapping("/usr/groupBuying/doWrite")
 	@ResponseBody
-	public String doWrite(HttpServletRequest req, String title, String body, String memberLocationTag, String buyingLocation) {
+	public String doWrite(HttpServletRequest req, String title, String body, String memberLocationTag,
+			String buyingLocation) {
 
 		Rq rq = (Rq) req.getAttribute("rq");
 
@@ -161,7 +162,6 @@ public class UsrGroupBuyingController {
 
 		return Ut.jsReplace(writeGroupBuyingRd.getResultCode(), writeGroupBuyingRd.getMsg(),
 				"../groupBuying/detail?id=" + id);
-00000
 	}
 
 	@RequestMapping("/usr/groupBuying/modify")
